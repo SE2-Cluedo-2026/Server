10886292f682b87983e11d691249d8767e35aae2
Vanesa Markovic <deine.uni.email@aau.at>
2026-04-11 22:01:03 +0200

    update CharacterType

--- src/main/java/at/aau/serg/websocketdemoserver/model/enums/CharacterType.java
+++ src/main/java/at/aau/serg/websocketdemoserver/model/enums/CharacterType.java
@@ -1,9 +1,8 @@
 package at.aau.serg.websocketdemoserver.model.enums;
 
 public enum CharacterType {
-    MISS_SCARLETT,
-    COLONEL_MUSTARD,
-    MRS_WHITE,
-    MR_GREEN,
-    MRS_PEACOCK
+    MRS_LAVENDER,
+    MRS_PINK,
+    DR_RED,
+    DR_BLUE
 }
