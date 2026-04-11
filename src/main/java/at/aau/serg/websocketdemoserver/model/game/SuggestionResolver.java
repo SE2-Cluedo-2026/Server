13836c4ebbd320f061c9a8f9dda71db2de794622
Vanesa Markovic <deine.uni.email@aau.at>
2026-04-11 12:09:47 +0200

    add SuggestionResolver class to game package

---
+++ src/main/java/at/aau/serg/websocketdemoserver/model/game/SuggestionResolver.java
@@ -0,0 +1,15 @@
+package at.aau.serg.websocketdemoserver.model.game;
+
+import java.util.List;
+
+public class SuggestionResolver {
+    public SuggestionResolver (){
+
+    }
+    public Player resolveSuggestion(Suggestion suggestion, List<Player> players) {
+
+        //TODO:
+
+        return null;
+    }
+}
