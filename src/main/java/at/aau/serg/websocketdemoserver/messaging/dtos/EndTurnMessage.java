a5baa09699a8c8dc3bcb5f516dcc679e359accbf
Vanesa Markovic <deine.uni.email@aau.at>
2026-04-12 19:02:04 +0200

    add classes to messaging.dtos

---
+++ src/main/java/at/aau/serg/websocketdemoserver/messaging/dtos/EndTurnMessage.java
@@ -0,0 +1,18 @@
+package at.aau.serg.websocketdemoserver.messaging.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class EndTurnMessage {
+    private String gameID;
+    private int previousPlayerIndex;
+    private int nextPlayerIndex;
+
+    public static EndTurnMessage from(String gameID, int previousPlayerIndex, int nextPlayerIndex) {
+        // TODO
+        return null;
+    }
+}
