4cae893a5bcedd53737589af4f25a91563154883
mucica33 <kocmut.tea@gmail.com>
2026-04-19 11:45:58 +0200

    Added initial network components

---
+++ src/main/java/at/aau/serg/websocketdemoserver/network/game/GameHandler.java
@@ -0,0 +1,6 @@
+package at.aau.serg.websocketdemoserver.network.game;
+
+public class GameHandler {
+    public void handleTurn() {
+    }
+}
---
+++ src/main/java/at/aau/serg/websocketdemoserver/network/lobby/LobbyHandler.java
@@ -0,0 +1,9 @@
+package at.aau.serg.websocketdemoserver.network.lobby;
+
+public class LobbyHandler {
+    public void handleJoin() {
+    }
+
+    public void handleLeave() {
+    }
+}
