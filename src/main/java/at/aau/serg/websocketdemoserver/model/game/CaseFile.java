88b2664dd215cf285a9d7ed7c004f4dd3106b7f0
Anna Robin <anrobin@edu.aau.at>
2026-05-08 17:16:15 +0200

    feat(casefile): implement case file creation

--- src/main/java/at/aau/serg/websocketdemoserver/model/game/CaseFile.java
+++ src/main/java/at/aau/serg/websocketdemoserver/model/game/CaseFile.java
@@ -10,14 +10,38 @@ public class CaseFile {
     private WeaponCard weaponCard;
 
     public CaseFile(SuspectCard suspectCard, RoomCard roomCard, WeaponCard weaponCard) {
+        create(suspectCard, roomCard, weaponCard);
+    }
+
+    public void create(SuspectCard suspectCard, RoomCard roomCard, WeaponCard weaponCard) {
         this.suspectCard = suspectCard;
         this.roomCard = roomCard;
         this.weaponCard = weaponCard;
     }
 
+    public void clear() {
+        this.suspectCard = null;
+        this.roomCard = null;
+        this.weaponCard = null;
+    }
+
+    public boolean isComplete() {
+        return suspectCard != null && roomCard != null && weaponCard != null;
+    }
+
+    public boolean matches(Accusation accusation) {
+        if (!isComplete() || accusation == null) {
+            return false;
+        }
+
+        return suspectCard.getSuspect() == accusation.getSuspect()
+                && roomCard.getRoom() == accusation.getRoom()
+                && weaponCard.getWeapon() == accusation.getWeapon();
+    }
+
+
     public boolean matches() {
-        // TODO
-        return false;
+        return isComplete();
     }
 
     public SuspectCard getSuspectCard() {
